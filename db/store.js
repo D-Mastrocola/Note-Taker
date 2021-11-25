a0f25d54764a538f0bab57250bf7af0b1e6aab4e
Dominic Mastrocola <60686549+D-Mastrocola@users.noreply.github.com>
2021-11-25 11:50:28 -0500

    finished routes

--- db/store.js
+++ db/store.js
@@ -1,47 +1,57 @@
 const util = require('util');
 const fs = require('fs');
+const path = require('path');
+const db = require('../db/db.json');
 
 // This package will be used to generate our unique ids. https://www.npmjs.com/package/uuid
 const uuidv1 = require('uuid/v1');
 
-const readFileAsync = util.promisify(fs.readFile);
-const writeFileAsync = util.promisify(fs.writeFile);
 
-
-class Store {
-  read() {
-    return readFileAsync('db/db.json', 'utf8');
-  }
-
-  write(note) {
-    
-  }
-
-  getNotes() {
-
-    // read and return notes
-  
-
-      // If notes isn't an array or can't be turned into one, send back a new empty array
-      
-  }
-
-  addNote(note) {
-    // creat note object with validation
-    
-
-    // Add a unique id to the note using uuid package
-    
-
-    // Get all notes, add the new note, write all the updated notes, return the newNote
-    
-  }
-
-  removeNote(id) {
-    // Get all notes, remove the note with the given id, write the filtered notes
-    
-  }
+function read() {
+  return readFileAsync('db/db.json', 'utf8');
 }
 
+function write(note) {
 
+}
+
+function getNotes() {
+
+  // read and return notes
+
+
+  // If notes isn't an array or can't be turned into one, send back a new empty array
+
+}
+function validateNote(note) {
+  if (!note.title || typeof note.title !== "string") {
+    return false;
+  }
+  if (!note.text || typeof note.text !== "string") {
+    return false;
+  }
+  return true;
+}
+function addNote(body, notesArray) {
+  const note = body;
+  // creat note object with validation
+  console.log(notesArray);
+  // Add a unique id to the note using uuid package
+  note.id = uuidv1();
+
+  // Get all notes, add the new note, write all the updated notes, return the newNote
+
+  notesArray.push(note);
+  fs.writeFileSync(
+    path.join(__dirname, '../db/db.json'),
+    JSON.stringify({ notes: notesArray }, null, 2)
+  )
+  return note;
+}
+
+function removeNote(id) {
+  // Get all notes, remove the note with the given id, write the filtered notes
+
+}
 // export
+module.exports = { read, write, getNotes, validateNote, addNote, removeNote };
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,30 +1,28 @@
 //Requires
 const router = require("express").Router();
-const db = require('../db/db.json');
-const {createNewNote} = require("../lib/note");
+const { notes } = require('../db/db.json');
+const { read, write, getNotes, validateNote, addNote, removeNote } = require('../db/store')
 
 // GET "/api/notes" responds with all notes from the database
 router.get("/notes", (req, res) => {
-  let results = db;
-  /*if (req.query) {
-    results = filterByQuery(req.query, results);
-  }*/
+  let results = notes;
   res.json(results);
 });
 
+
+
 // Post "/api/notes" responds with adding a note to database
 router.post("/notes", (req, res) => {
-  // set id based on what the next index of the array will be
-  req.body.id = db.length.toString();
 
   // if any data in req.body is incorrect, send 400 error back
-  if (!validateAnimal(req.body)) {
+  if (!validateNote(req.body)) {
     res.status(400).send("The note is not properly formatted.");
   } else {
-    const note = createNewNote(req.body, animals);
-
+    console.log('adding note');
+    const note = addNote(req.body, notes);
     res.json(note);
   }
+  res.status(200).send('finished')
 });
 
 // DELETE "/api/notes" deletes the note with an id equal to req.params.id
